f9ad3c6b59acdc08c66d29f444cdff1d97f15189
AlexDevelop1991 <alexdevelop1991@gmail.com>
2023-03-20 18:39:41 +0200

    homework 4

--- homework/homework4/game.py
+++ homework/homework4/game.py
@@ -1,15 +1,15 @@
-welcome = 'Привет добро пожаловать в игру  Угадай слово'
-question = 'Вопрос:Какой самый большой океан в мире?'
-print(welcome)
-print(question)
 
-variant = (input('Вводите ответ с большой буквы ваш вариант:'))
-answer = 'Тихий'
+print('Привет, добро пожаловать в игру "Угадай слово"')
+print('Вопрос: Какой самый большой океан в мире?')
+
+variant = input('Ваш вариант:').lower()
+
+answer = 'тихий'
 
 while variant != answer:
     if answer < variant:
         print('Не правильно попробуй ещё раз ')
-    variant = input('Новый вариант:')
+    variant = input('Новый вариант:').lower()
 
 print('Поздравляю правильный ответ!')
 
