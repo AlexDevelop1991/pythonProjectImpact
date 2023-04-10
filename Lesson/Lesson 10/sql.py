d5ee99c77df83f967d063681e9708b9d2ecd4478
AlexDevelop1991 <alexdevelop1991@gmail.com>
2023-04-10 20:32:49 +0300

    l11

--- Lesson/Lesson 10/sql.py
+++ Lesson/Lesson 10/sql.py
@@ -16,17 +16,38 @@ try:
     table = """CREATE TABLE Impact(
                Email VARCHAR(255) NOT NULL,
                First_Name CHAR(25) NOT NULL,
-               Last_Name CHAR(25));"""
+               Last_Name CHAR(25));
+               """
     cursor.execute(table)
     print('Table is Ready')
-    cursor.execute('''INSERT INTO Impact VALUES (stars91@gmail.com, Alexander, Brovco)''')
-    cursor.execute('''INSERT INTO Impact VALUES (, Alexander, )''')
-    cursor.execute('''INSERT INTO Impact VALUES (, Brovco)''')
+    cursor.execute('''INSERT INTO Impact VALUES ("stars91@gmail.com", "Alexander", "Brovco");''')
+    cursor.execute('''INSERT INTO Impact VALUES ("blabla@gmail.com", "Alexander", "A");''')
+    cursor.execute('''INSERT INTO Impact VALUES ("last@gmail.com", "Brovco", "B");''')
     # Display data inserted
     print('Data Insert in the table:')
-    data = cursor.execute('''SELECT * FROM Impact''')
+    data = cursor.execute("""SELECT * FROM Impact
+                          WHERE First_Name = 'Alexander'""")
     for row in data:
         print(row)
+
+    update = cursor.execute("""UPDATE Impact SET Email = 'shadowstars@gmail.com' WHERE First_Name = 'Alexander'""")
+
+    all_data = cursor.execute("""SELECT * FROM Impact""")
+    print("Table Impact after Update:")
+    for row in all_data:
+        print(row)
+
+    delete = cursor.execute("""DELETE FROM Impact WHERE First_Name = 'Alexander'""")
+
+    delete_data = cursor.execute("""SELECT * FROM Impact""")
+    print("Table Impact after Delete:")
+    for row in delete_data:
+        print(row)
+
+
+
+
+
 # Commit your changes in the database
     sqliteConnection.commit()
     # Close the cursor
---
+++ Lesson/Lesson 11/Kivy.py
@@ -0,0 +1,2 @@
+from kivymd.app import MDApp
+from kivy.uix.label import MDLabel
