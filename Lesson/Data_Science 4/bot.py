19b68bdc25a5cde007df8b5e887e58669a8961d2
AlexDevelop1991 <alexdevelop1991@gmail.com>
2023-05-20 19:33:52 +0300

    data_sc

--- Lesson/Data_Science 4/bot.py
+++ Lesson/Data_Science 4/bot.py
@@ -1,11 +1,48 @@
-import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+a = input('Выберите язык/Choose language (ru/en): ')
 
-a = input('Выберите язык:')
 
-data = pd.read_csv('https://raw.githubusercontent.com/amankharwal/Website-data/master/dataset.csv')
+while True:
+
+    if a == 'ru':
+        first_q = 'Как ты себя чувствуешь?'
+        print(first_q)
+        a1 = input()
+        print('Отлично 😊')
+        second_q = 'Что ты сегодня делал?'
+        print(second_q)
+        a2 = input()
+        print('Очень хорошо что у вас был такой насыщенный день😎!!!')
+        third_q = 'Какая твоя любимая музыка?'
+        print(third_q)
+        a3 = input()
+        print('Мне тоже такая музыка нравиться🎵))')
+        answer = input('Хотите ещё раз пообщаться?🙂 да или нет:')
+        if answer == 'нет':
+            break
+
+    elif a == 'en':
+        first_q = 'How do you feel?'
+        print(first_q)
+        a1 = input()
+        print('Perfect 😊')
+        second_q = 'What you were doing today?'
+        print(second_q)
+        a2 = input()
+        print("That's great that you had such a wonderful day😎!!!")
+        third_q = 'What is your favourite music ?'
+        print(third_q)
+        a3 = input()
+        print('I like suck music too🎵')
+        answer = input('Do you want to chat again?🙂 yes or no:')
+        if answer == 'no':
+            break
+
+
+
+
+
+
+
+
 
 
