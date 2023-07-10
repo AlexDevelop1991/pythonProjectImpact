be028e7b3ea657162cbe6d54b85a189417b1a4f1
AlexDevelop1991 <alexdevelop1991@gmail.com>
2023-07-10 14:53:13 +0300

    chat_bot

---
+++ Lesson/Chat_Bot _6/data_base/inference.py
@@ -0,0 +1,9 @@
+import sqlite3 as sq
+import pandas as pd
+
+
+def get_df_from_db(cuisine):
+    db = sq.connect('recipes.db')
+    sql_query = "SELECT title, instructions, ingredients_query" \
+                "FROM main_recipes WHERE cuisine = ?"
+    return pd.read_sql(sql_query, db, params=(cuisine,))
